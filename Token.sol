8d0bbad60cf54a8c8295db02be37a5bb8ac70b45
Hyeong <53553469+hyeongga@users.noreply.github.com>
2023-06-14 00:49:25 +0900

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -1,30 +1,48 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.18;
 
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
-contract BBlock is ERC20("BaeBlock","BB"){
+// 토큰 구매 가능
+// 토큰 가격은 1 BBL = 1000원
+// 최소 1천원 단위로 구매가능
+// 토큰으로 거래시 1% 리워드
+// 컨트랙트에 토큰을 쌓아두고 거래
 
-    //발행량 정해야함 (계속 배포 가능하게? 한정된 수량? 운영을 어떻게 할지 구체적으로 구상필요)
-    //발행된 토큰이 버튼 누른 사람의 지갑으로 들어감
-    constructor(uint _totalSupply){
-        _mint(msg.sender, _totalSupply);
-    }
+
+contract BBlock is ERC20("BaeBlock","BB"){
     
-    //버튼 누른 사람의 잔고 조회
-    function getBalance() public view returns(uint){
-        return balanceOf(msg.sender);
+    address private C_owner;
+
+    //초기 오너설정 및 토큰 발행
+    constructor(uint _initialSupply) {
+        _mint(address(this), _initialSupply);
+        C_owner = msg.sender;
     }
+
+    //추가 토큰 발행 ; buyToken, getReward에 따라 자동으로 발행되도록 수정?
+    function MintToken(uint _a) public {
+        require(C_owner == msg.sender, "You can't mint Token");
+        _mint(address(this), _a);
+    } 
 
     function decimals() public pure override returns (uint8) {
-        return 0;
-    }
-    
-    //발행한 토큰이 스마트 컨트랙트에 저장
-    function MintToken(uint a) public {
-        _mint(address(this), a);
+        return 3;
     }
     
     receive() external payable{}
+
+    //토큰으로 거래시 1% 리워드 ; internal? 
+    // _amountBB :  거래시 지불한 토큰금액 
+    function Reward(uint _amountBB) public {
+        _transfer(address(this), msg.sender, _amountBB/100);
+    }
+
+    // 지불구조 미완성
+    function buyToken(uint _amountKRW) public payable {
+        require(balanceOf(msg.sender) > 0, "" ); //ether잔고 조회
+        _transfer(address(this), msg.sender, _amountKRW/1000);
+    }
+
 
 }
