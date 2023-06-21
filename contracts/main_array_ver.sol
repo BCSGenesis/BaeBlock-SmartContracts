2f3eeb53f448bde8384c2c497076ed27990be5f5
Prkskhn <tjrgns7452@gmail.com>
2023-06-21 21:41:58 +0900

    truffle

--- contracts/main_array_ver.sol
+++ contracts/main_array_ver.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.19;
 
-contract Payment {
+contract delivery_a {
 
     address public owner;
 
---
+++ migrations/1_main_array.js
@@ -0,0 +1,5 @@
+const A = artifacts.require("delivery_a");
+
+module.exports = function (deployer) {
+  deployer.deploy(A);
+};
