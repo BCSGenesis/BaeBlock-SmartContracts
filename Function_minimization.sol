ec75e9c315dd4f5e379a3eec2b9994dca1d68d86
Hyeong <53553469+hyeongga@users.noreply.github.com>
2023-06-28 19:05:19 +0900

    Reflect h662's feedback
    ✅ store, driver의 구조체, 회원가입 함수 하나로 통합
    ✅ 주문번호 uint로 따로 지정 & order함수에서 자체적으로 계산
    ✅ store, customer 배달비 하나로 통합
    ✅ 배달 한 건에 집중한 코드로 작성
    (추후 3건으로 작성시 mapping value값 uint사용 추천)
    ✅ 돈 지불 함수 따로 추가
    
    <미해결>
    - 가게 거절시, 고객에게 환불
    - 고객이 완료 버튼 누르면 가게와 배달원에게 돈 지급

--- Function_minimization.sol
+++ Function_minimization.sol
@@ -3,27 +3,22 @@ pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Payment /*is Ownable ; 수정필요*/ {
+contract Payment is Ownable {
 
 // 변수선언 -----------------------------------------------------------------------
-    address public owner;
-
-    //컨트랙트 주인설정
-    constructor(){
-        owner = msg.sender;
-    }
-
+    
+    //주문 번호
+    uint orderID;       
+    
     //주문 구조체
     struct Order {
-        uint orderID;
-        address cWallet;
-        address sWallet;
-        address rWallet;
-        uint foodPrice;
-        uint cDeliveryFee;
-        uint sDeliveryFee;
-        uint deliveryTip;
-        orderState status;
+        address cWallet;    //고객 지갑
+        address sWallet;    //가게 지갑
+        address rWallet;    //라이더 지갑
+        uint foodPrice;     //음식 가격
+        uint deliveryFee;   //거리에 따라 지정되는 배달비
+        uint deliveryTip;   //배달팁
+        orderState status;  //주문 상태
     }
 
     //주문상태
@@ -32,29 +27,13 @@ contract Payment /*is Ownable ; 수정필요*/ {
         store_accept,
         store_decline,
         store_cookFinish,
-        rider_assigned,
+        rider_accept,
         rider_inDelivery,
         rider_deliveryComplete
     }
 
-    //주문번호로 주문 조회
-    mapping(uint => Order) searchOrder; //완료된 주문 제거?
-    //배달원별 배달목록
-    mapping(address => Order[3]) deliveryList;
-
-    //가게 점주 입장 구조체
-    struct Store {
-        address storeWallet;
-        Role role;
-        // address NFT;//수정필요
-    }
-
-    //배달원 구조체
-    struct Rider {
-        address riderWallet;
-        Role role;
-        // address NFT;//수정필요
-    }
+    //주문번호로 주문 조회 (orderID=>Order)
+    mapping(uint => Order) searchOrder; 
 
     //역할
     enum Role{
@@ -62,82 +41,92 @@ contract Payment /*is Ownable ; 수정필요*/ {
         rider
     }
 
-    //배달원 목록
-    mapping(address => Rider) riders;
-    //가게 점주 목록
-    mapping(address => Store) stores;
+    //회원 구조체
+    struct Member {
+        address Wallet;
+        Role role;
+    }
+
+    //회원 목록
+    mapping(address => Member) members;
 
 // 정보등록 -----------------------------------------------------------------------
-    //가게(점주) 회원가입
-    function storeRegist() public  {
-        stores[msg.sender] = Store(msg.sender, Role.store);
-    }
-    //배달원 회원가입
-    function riderRegist() public {
-        riders[msg.sender]= Rider(msg.sender, Role.rider);
+
+    //회원가입
+    function Register(uint _role) public {
+        if(_role==0){
+            members[msg.sender]= Member(msg.sender, Role.store);
+        }else if(_role==1){
+            members[msg.sender]= Member(msg.sender, Role.rider);
+        }
     }
 
 // 결제 ------------------------------------------------------------------------------
 
     //고객이 주문하고 돈을 보냄
-    function ordering(uint _orderID, address _sWallet, uint _foodPrice, uint _cDeliveryFee, uint _sDeliveryFee, uint _deliveryTip) public payable{
-        require(msg.value == (_foodPrice + _cDeliveryFee + _deliveryTip));
-        searchOrder[_orderID] = Order( _orderID, msg.sender, _sWallet, address(0) ,_foodPrice, _cDeliveryFee, _sDeliveryFee, _deliveryTip, orderState.order);
+    function ordering(address _sWallet, uint _foodPrice, uint _deliveryFee, uint _deliveryTip) public payable{
+        require(msg.value == (_foodPrice + _deliveryFee + _deliveryTip));           //잔고 확인
+        require(members[_sWallet].role == Role.store);                              //사용가능한 가게인지 확인
+        searchOrder[orderID] = Order(msg.sender, _sWallet, address(0) ,_foodPrice, _deliveryFee, _deliveryTip, orderState.order);
+        orderID++;
     }
 
     //가게 : 주문 수락시 배달비 지불 / 거절시 지불 안됨
     function storeAccept_Decline(uint _orderID, bool) public payable {
-        require(riders[msg.sender].role == Role.store);
-        require(msg.sender ==  searchOrder[_orderID].sWallet, "You can't access this order");
-        require(searchOrder[_orderID].status == orderState.order, "You can't access this order");
-        // require(/*NFT사용조건문*/,"You shound buy NFT"?); //수정 필요
+        require(msg.sender == searchOrder[_orderID].sWallet, "You can't access this order.");           //가게 주인확인
+        require(searchOrder[_orderID].status == orderState.order, "This order is not available.");      //주문 상태 확인
         if(true){
-            searchOrder[_orderID].status = orderState.store_accept;        
-            require(msg.value == searchOrder[_orderID].sDeliveryFee, "You should pay DeliveryFee");
+            require(msg.value == searchOrder[_orderID].deliveryFee, "You should pay DeliveryFee");      //배달비 확인
+            searchOrder[_orderID].status = orderState.store_accept;                                     //주문 수락
         }else{
-            searchOrder[_orderID].status = orderState.store_decline;      
+            searchOrder[_orderID].status = orderState.store_decline;                                    //주문거절
+            uint refundPrice = searchOrder[orderID].foodPrice + searchOrder[orderID].deliveryFee + searchOrder[orderID].deliveryTip;                     
+            payable (searchOrder[orderID].cWallet).transfer(refundPrice);                               //환불  //??????????컨트랙트가 고객에게 확인! transferfrom사용해야할텐데....?
         }
     }
 
-    //가게 음식조리완료 : 고객이 지불한 돈 받음
+    //가게 음식조리완료
     function cookFinish(uint _orderID) public payable {
-        require(msg.sender == searchOrder[_orderID].sWallet && searchOrder[_orderID].status == orderState.order, "You can't access this order");
+        require(msg.sender == searchOrder[_orderID].sWallet);                    //가게 주인확인
+        require(searchOrder[_orderID].status == orderState.store_accept);
         searchOrder[_orderID].status = orderState.store_cookFinish;
-        payable (searchOrder[_orderID].sWallet).transfer(searchOrder[_orderID].foodPrice); //수수료 부과 필요
     }
 
     //배달원 지정
-    function getDelivery(uint _orderID) public {
-        require(riders[msg.sender].role == Role.rider);                         //배달기사 회원가입여부 확인
-        require(searchOrder[_orderID].rWallet ==  address(0));                  //배달기사 지정 안된 상태인지 확인
-        require(searchOrder[_orderID].status == orderState.store_accept);       //가게가 주문 받은 상태인지 확인
-        // require( /*&&NFT확인*/);
-
+    function setDelivery(uint _orderID) public {
+        require(members[msg.sender].role == Role.rider);                         //배달기사 회원가입여부 확인
+        require(searchOrder[_orderID].rWallet == address(0));                    //배달기사 지정 안된 상태인지 확인
+        require(searchOrder[_orderID].status == orderState.store_cookFinish);       //가게가 주문 받은 상태인지 확인
         searchOrder[_orderID].rWallet = msg.sender;
-        searchOrder[_orderID].status = orderState.rider_assigned;
-        deliveryList[msg.sender][0] = searchOrder[_orderID]; //정적배열 수정필요
+        searchOrder[_orderID].status = orderState.rider_accept;
     }
 
-    //배달시작  //제거 고민 중
-    function setInDelivery(uint _orderID)public{
+    //배달시작
+    function startDelivery(uint _orderID)public{
         require(searchOrder[_orderID].rWallet == msg.sender);
-        require(searchOrder[_orderID].status == orderState.store_cookFinish);
+        require(searchOrder[_orderID].status == orderState.store_cookFinish );
         searchOrder[_orderID].status = orderState.rider_inDelivery;
     }
 
-    //배달이 완료 : 배달비 수령
-    function setDeliveryDone(uint _orderID) public {
-        require(searchOrder[_orderID].rWallet == msg.sender && searchOrder[_orderID].status == orderState.rider_inDelivery);
-        uint totalFee = searchOrder[_orderID].cDeliveryFee + searchOrder[_orderID].sDeliveryFee + searchOrder[_orderID].deliveryTip;
-        payable(msg.sender).transfer(totalFee);
-        
-        //deliveryList[msg.sender][0] = searchOrder[_orderID];//배달 완료된 항목은 배달리스트에서 제거
+    //배달완료
+    function doneDelivery(uint _orderID) public {
+        require(searchOrder[_orderID].rWallet == msg.sender);
+        require(searchOrder[_orderID].status == orderState.rider_inDelivery);    
+        searchOrder[_orderID].status = orderState.rider_deliveryComplete;
     }
 
-    //컨트랙트 돈 인출 //ownable 상속 후 수정
-    function withdraw(uint _money) public{
+    //거래 완료 후, 가게와 배달원에게 돈 입금
+    function orderComplete(uint _orderID, bool) public {
+        uint total = (searchOrder[_orderID].deliveryFee *2) + searchOrder[_orderID].deliveryTip;
+        // if() : 고객이 음식받음 버튼을 누르면 지급됨 (일정시간이 지나거나 owner가 눌러줘도 가능하도록)
+        payable(searchOrder[_orderID].rWallet).transfer(totalFee); 
+        payable(searchOrder[_orderID].sWallet).transfer(searchOrder[_orderID].foodPrice); //수수료 부과 필요
+    }
+    
+    //컨트랙트 돈 인출 
+    function withdraw(uint _money) public onlyOwner{
         uint possible = address(this).balance -0/*출금불가한 금액*/;
-        require (msg.sender == owner && possible >0);
-        payable(owner).transfer(_money);
+        require(possible >0);
+        payable(msg.sender).transfer(_money);
     }
 }
