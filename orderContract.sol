00635cf3f4c45b7240249a22ef178ffd4ef42993
Prkskhn <tjrgns7452@gmail.com>
2023-07-04 21:29:36 +0900

    Link Nft complete

--- orderContract.sol
+++ orderContract.sol
@@ -3,17 +3,22 @@ pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./HS-rider.sol";
+import "./HS-store.sol";
 
-contract Payment is Ownable,delivery {
 
+contract Payment is Ownable {
 // HS-rider 가져오기-----------------------------------------------------------------
    
     delivery riderContract;
+    store Store;
 
-    constructor(address _riderContractAddr){
-        riderContract= delivery(_riderContractAddr);
-
+    constructor(address _riderContractAddr,address _storeContractAddr){
+        riderContract= delivery(payable (_riderContractAddr));
+        Store= store(payable (_storeContractAddr));
     }
+
+    uint platformFee = 2;   //플랫폼 수수료
+    event Result(string message);
 
 // 변수선언 -----------------------------------------------------------------------
     
@@ -80,7 +85,7 @@ contract Payment is Ownable,delivery {
 // 결제 ------------------------------------------------------------------------------
     //고객이 주문하고 돈을 보냄
     function ordering(address _sWallet, uint _foodPrice, uint _deliveryFee, uint _deliveryTip) public payable returns(uint){
-        require(msg.value == (_foodPrice + _deliveryFee + _deliveryTip));           //잔고 확인
+        require(msg.value == (_foodPrice + _deliveryFee + _deliveryTip)*1 ether);           //잔고 확인
         require(members[_sWallet].role == Role.store);                              //사용가능한 가게인지 확인
         searchOrder[orderID] = Order(msg.sender, _sWallet, address(0) ,_foodPrice, _deliveryFee, _deliveryTip, orderState.order);
         orderID++;
@@ -96,14 +101,17 @@ contract Payment is Ownable,delivery {
         require(msg.sender == searchOrder[_orderID].sWallet, "You can't access this order.");           //가게 주인확인
         require(searchOrder[_orderID].status == orderState.order, "This order is not available.");      //주문 상태 확인
         if(_bool==true){
-            require(msg.value == (searchOrder[_orderID].deliveryFee), "You should pay DeliveryFee");     //배달비 확인
+            require(msg.value == (searchOrder[_orderID].deliveryFee)*1 ether, "You should pay DeliveryFee");  //배달비 확인
             searchOrder[_orderID].status = orderState.store_accept;                                      //주문 수락
         }else{
             searchOrder[_orderID].status = orderState.store_decline;                                     //주문거절
             uint refundPrice = searchOrder[_orderID].foodPrice + searchOrder[_orderID].deliveryFee + searchOrder[_orderID].deliveryTip;
-            payable (searchOrder[_orderID].cWallet).transfer(refundPrice);                               //환불 
+            payable (searchOrder[_orderID].cWallet).transfer(refundPrice*1 ether);                               //환불 
         }
     }
+
+    
+
 
     //orderState ; 확인
     function returnOrderState(uint _orderID)public view returns(orderState){
@@ -116,19 +124,26 @@ contract Payment is Ownable,delivery {
         require(searchOrder[_orderID].status == orderState.store_accept);
         searchOrder[_orderID].status = orderState.store_cookFinish;
     }
+
+
     
+    
+    // function getRiderNftTime(address _a)public  view returns(uint){
+    //     return riderContract.getRiderNftTime(_a);
+    // }
+
     //web3에서 HS-rider컨트랙트의 getRiderNftTime(address)-getBlockTimeStamp() 값을 usestate변수로 받아 남은 일수 표현
     function getBlockTimeStamp()public view returns(uint){
         return block.timestamp;
     }
 
     //배달원 지정
-    function setDelivery(uint _orderID) public {
+    function setDelivery(uint _orderID) public  {
         require(members[msg.sender].role == Role.rider);                         //배달기사 회원가입여부 확인
         //이 배달기사가 배달 권한 nft를 가졌는지
         require(riderContract.getRiderNftTime(msg.sender)>=block.timestamp,"You don't have deliveryNft or Nft is expired");
         require(searchOrder[_orderID].rWallet == address(0));                    //배달기사 지정 안된 상태인지 확인
-        require(searchOrder[_orderID].status == orderState.store_cookFinish);       //가게가 주문 받은 상태인지 확인
+        require(searchOrder[_orderID].status == orderState.store_cookFinish); //가게가 주문 받은 상태인지 확인 
         searchOrder[_orderID].rWallet = msg.sender;
         searchOrder[_orderID].status = orderState.rider_accept;
     }
@@ -139,7 +154,7 @@ contract Payment is Ownable,delivery {
     }
 
     //배달권 민팅은 원본컨트랙트에서 실행해야하는듯..msg.value가 전달이 안됨..
-
+    
     //배달시작
     function startDelivery(uint _orderID)public{
         require(searchOrder[_orderID].rWallet == msg.sender);
@@ -154,15 +169,27 @@ contract Payment is Ownable,delivery {
         searchOrder[_orderID].status = orderState.rider_deliveryComplete;
     }
 
+    
     //거래 완료 후, 가게와 배달원에게 돈 입금
-    function orderComplete(uint _orderID, bool) public {
-        require(searchOrder[_orderID].status == orderState.rider_deliveryComplete);
+    function orderComplete2(uint _orderID, bool) public {
+        
+        // store Store = new store();                                            //Genesis.sol 파일 인스턴스화
+        uint nftOwner = Store.getMappingAccount();                            //tokenId가 저장되는 매핑 함수               
         require(searchOrder[_orderID].cWallet == msg.sender); // 고객이 음식받음 버튼을 누르면 지급됨
         uint totalFee = (searchOrder[_orderID].deliveryFee *2) + searchOrder[_orderID].deliveryTip;
         // + 일정시간이 지나거나 owner가 눌러줘도 가능하도록
         payable(searchOrder[_orderID].rWallet).transfer(totalFee); 
-        payable(searchOrder[_orderID].sWallet).transfer(searchOrder[_orderID].foodPrice); //수수료 부과 필요
-        searchOrder[_orderID].status = orderState.done;
+
+        if(nftOwner > block.timestamp ) { 
+            emit Result("You have an NFT. Platform fees are waived.");
+            payable(searchOrder[_orderID].sWallet).transfer(searchOrder[_orderID].foodPrice); 
+            searchOrder[_orderID].status = orderState.done;
+        } else{ 
+             emit Result("You don't have an NFT or your usage period has expired.  The platform fee is 2% of the foodprice.");
+             uint FoodPrice = searchOrder[_orderID].foodPrice;
+             payable(searchOrder[_orderID].sWallet).transfer(FoodPrice-(FoodPrice * platformFee) /100); //수수료 부과
+             searchOrder[_orderID].status = orderState.done;
+        }
     }
 
     //컨트랙트 돈 인출 
@@ -171,5 +198,4 @@ contract Payment is Ownable,delivery {
         require(possible >0);
         payable(msg.sender).transfer(_money);
     }
-
 }
